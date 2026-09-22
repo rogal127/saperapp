f0e64fc087044d3eda37a00c76b66a66edfc791b
Patryk Rogala <p.rogala@savicki.pl>
2026-09-22 16:08:12 +0200

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
+import { nativephpMobile, nativephpHotFile } from './vendor/nativephp/mobile/resources/js/vite-plugin.js';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            hotFile: nativephpHotFile(),
             fonts: [
                 bunny('Instrument Sans', {
                     weights: [400, 500, 600],
@@ -15,6 +17,7 @@ export default defineConfig({
             ],
         }),
         tailwindcss(),
+        nativephpMobile(),
     ],
     server: {
         watch: {
